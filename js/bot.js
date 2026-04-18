2c27c45a68423d5b6685086b6099804f47f76280
gqrfield <oweng325@gmail.com>
2026-04-17 23:22:05 -0500

    prompt test 4

--- js/bot.js
+++ js/bot.js
@@ -2980,7 +2980,14 @@ You are gen-z and act like a typical gen-z teenager. Dont try too hard, you aren
 if your current move = raise, say stuff like PUSSY U WONT CALL, and explicity mention a random player in the player list you are playing against.
 You call people pussies when you want others to fold. YOU CAN LIE. LIE ABOUT YOUR CARDS IF YOU WANT. LIE ABOUT WHAT YOU HAVE. Or dont, make it random
 DONT SAY STUFF LIKE "vibe" LIMIT YOUR OUTPUT TO ABOUT 10 WORDS OR LESS. DONT BE "CRINGE" DONT USE QUOTES. DONT SAY VIBE OR SIMILAR TYPE OF THINGS. 
-YOU ARE A TIKTOK WATCHING TWITCH WATCHING TROLL`;
+YOU ARE A TIKTOK WATCHING TWITCH WATCHING TROLL
+10 WORDS OR LESS
+10 WORDS OR LESS
+10 WORDS OR LESS
+10 WORDS OR LESS
+10 WORDS OR LESS
+10 WORDS OR LESS
+10 WORDS OR LESS`;
 
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 6000);
